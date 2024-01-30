4a8036f428a0dae4f8c7a4ea75b236abb64e15e5
JNzH <jnzh0x@gmail.com>
2024-01-30 13:31:40 -0500

    change to 2021

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -13,18 +13,7 @@ import (
 )
 import "strconv"
 
-//
-// example to show how to declare the arguments
-// and reply for an RPC.
-//
-
-type ExampleArgs struct {
-	X int
-}
-
-type ExampleReply struct {
-	Y int
-}
+// Add your RPC definitions here.
 
 type TaskType int
 
@@ -76,13 +65,11 @@ func DebugTask(label string, t *MRTask) {
 	fmt.Printf("========================\n")
 }
 
-// Add your RPC definitions here.
-
 // Cook up a unique-ish UNIX-domain socket name
-// in /var/tmp, for the master.
+// in /var/tmp, for the coordinator.
 // Can't use the current directory since
 // Athena AFS doesn't support UNIX-domain sockets.
-func masterSock() string {
+func coordinatorSock() string {
 	s := "/var/tmp/824-mr-"
 	s += strconv.Itoa(os.Getuid())
 	return s
